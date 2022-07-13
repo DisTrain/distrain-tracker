5e283459e62e3f3dd8f4f28081aef175e0430be5
TarekSamy9 <tsamy@ogoul.com>
2022-07-13 09:39:12 +0200

    merge with gui

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ deviceRepo.resetAllDevicesStatus();
 const requestListener = function (req: IncomingMessage, res: ServerResponse) {
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
-  console.log(req.url,req.method)
+  console.log(req.url, req.method);
   if (req.url === "/task" && req.method === "POST") {
     /*
 			1- save task in db
@@ -97,14 +97,14 @@ const requestListener = function (req: IncomingMessage, res: ServerResponse) {
         await schedule();
         res.end(JSON.stringify(response));
       });
-  }else if(req.url == "/devices" && req.method === "GET"){
+  } else if (req.url == "/devices" && req.method === "GET") {
     req
-    .on("data",()=>{})
-    .on("end", async () => {
-      const response = await deviceRepo.getAllDevices();
-      res.end(JSON.stringify(response));
-    });
-  }else if(req.url == "/task2" && req.method === "POST"){
+      .on("data", () => {})
+      .on("end", async () => {
+        const response = await deviceRepo.getAllDevices();
+        res.end(JSON.stringify(response));
+      });
+  } else if (req.url == "/task2" && req.method === "POST") {
     const size: number = parseInt(req.headers["content-length"]!, 10);
     const buffer = Buffer.allocUnsafe(size);
     let pos: number = 0;
@@ -115,7 +115,7 @@ const requestListener = function (req: IncomingMessage, res: ServerResponse) {
       })
       .on("end", async () => {
         const data = JSON.parse(buffer.toString());
-        console.log(data)
+        console.log(data);
         let task: Task = {
           //id: uuid(),
           id: data.task_name,
@@ -180,7 +180,7 @@ wss.on("connection", async (ws, req) => {
       status: "idle",
       last_login: new Date(Date.now()),
       address: deviceAddress,
-      memMeg: availableMemMegs
+      memMeg: availableMemMegs,
     });
     ws.send(JSON.stringify({ type: "deviceId", data: deviceId }));
   } else {
@@ -222,7 +222,7 @@ async function schedule() {
   if (minTask) {
     const idleDevices = await deviceRepo.getNIdleDevices(minTask.devices_count); // Should be connected devices and not busy
     const devicesList = idleDevices.map((d, i) => ({ number: i, ...d }));
-    console.log(devicesList)
+    console.log(devicesList);
     let socket;
     if (idleDevices.length >= minTask.devices_count) {
       // Generate URL for metadata file
@@ -234,9 +234,9 @@ async function schedule() {
 
       for (let [i, dev] of devicesList.entries()) {
         //  Get device socket
-        console.log("dev1",dev);
+        console.log("dev1", dev);
         let otherDevicesList = devicesList.map((d) => ({ number: d.number, address: d.address })).sort((a, b) => a.number - b.number);
-        console.log(otherDevicesList)
+        console.log(otherDevicesList);
         socket = deviceRepo.getSocket(dev.id);
 
         // Prepare the message
@@ -255,12 +255,12 @@ async function schedule() {
           data_type_params: minTask.dataTypeParams,
           devices_list: otherDevicesList,
         };
-        console.log("data",data)
+        console.log("data", data);
         if (socket) {
           // Send the work message
           const msg = new WorkMessage(socket, JSON.stringify(data));
           msg.handle();
-					await deviceRepo.setStatus(dev.id, "busy");
+          await deviceRepo.setStatus(dev.id, "busy");
           // construct device mesh of network
           await deviceRepo.connectDeviceToTask(dev.id, minTask.id, dev.number);
           // connect working devices to their task
@@ -276,64 +276,63 @@ async function schedule() {
   }
 }
 
-async function schedule2(data:any) {
-    const devicesList = data.devs.map((d:any, i:number) => ({ number: i, ...d,address:d.ip }));
-    //console.log(devicesList)
-    let socket;
-    // Generate URL for metadata file
+async function schedule2(data: any) {
+  const devicesList = data.devs.map((d: any, i: number) => ({ number: i, ...d, address: d.ip }));
+  //console.log(devicesList)
+  let socket;
+  // Generate URL for metadata file
+  const command = new GetObjectCommand({
+    Bucket: process.env.S3_BUCKET,
+    Key: `${data.task_name}/${data.model_name}`,
+  });
+  const metadataUrl = await getSignedUrl(s3, command, { expiresIn: Number(process.env.S3_URL_EXPIRY) });
+
+  const commandTFLite = new GetObjectCommand({
+    Bucket: process.env.S3_BUCKET,
+    Key: `${data.task_name}/${data.tf_lite_metadata}`,
+  });
+  const metadataTFLite = await getSignedUrl(s3, commandTFLite, { expiresIn: Number(process.env.S3_URL_EXPIRY) });
+
+  for (let [i, dev] of devicesList.entries()) {
+    //console.log("dev",dev);
+    //  Get device socket
+    let otherDevicesList = devicesList.map((d: any) => ({ number: d.number, address: d.address })).sort((a: any, b: any) => a.number - b.number);
+    //console.log("other",otherDevicesList)
+    socket = deviceRepo.getSocket(dev.id);
+
+    // Prepare the message
     const command = new GetObjectCommand({
       Bucket: process.env.S3_BUCKET,
-      Key: `${data.task_name}/${data.model_name}`,
+      Key: `${data.task_name}/${data.devs[i].chunk}.zip`,
     });
-    const metadataUrl = await getSignedUrl(s3, command, { expiresIn: Number(process.env.S3_URL_EXPIRY) });
+    const chunkUrl = await getSignedUrl(s3, command, { expiresIn: Number(process.env.S3_URL_EXPIRY) });
 
-    const commandTFLite = new GetObjectCommand({
-      Bucket: process.env.S3_BUCKET,
-      Key: `${data.task_name}/${data.tf_lite_metadata}`,
-    });
-    const metadataTFLite = await getSignedUrl(s3, commandTFLite, { expiresIn: Number(process.env.S3_URL_EXPIRY) });
-
-    for (let [i, dev] of devicesList.entries()) {
-      //console.log("dev",dev);
-      //  Get device socket
-      let otherDevicesList = devicesList.map((d:any) => ({ number: d.number, address: d.address })).sort((a:any, b:any) => a.number - b.number);
-      //console.log("other",otherDevicesList)
-      socket = deviceRepo.getSocket(dev.id);
-
-      // Prepare the message
-      const command = new GetObjectCommand({
-        Bucket: process.env.S3_BUCKET,
-        Key: `${data.task_name}/${data.devs[i].chunk}.zip`,
-      });
-      const chunkUrl = await getSignedUrl(s3, command, { expiresIn: Number(process.env.S3_URL_EXPIRY) });
-
-      const data1 = {
-        task_id: data.task_name,
-        number: dev.number,
-        metadata_url: metadataUrl,
-        chunk_url: chunkUrl,
-        data_type: "csv",
-        data_type_params: "a=k",
-        devices_list: otherDevicesList,
-        is_tf_lite: data.is_tf_lite,
-        tf_lite_metadata:metadataTFLite
-      };
-      //console.log("data1",data1)
-      if (socket) {
-        // Send the work message
-        const msg = new WorkMessage(socket, JSON.stringify(data1));
-        msg.handle();
-        // construct device mesh of network
-        await deviceRepo.connectDeviceToTask(dev.id, data.task_name, dev.number);
-        // connect working devices to their task
-        const otherDevicesId: string[] = devicesList.filter((d:any) => d.id !== dev.id).map((d:any) => d.id);
-        await deviceRepo.makeDevicesMesh(dev.id, otherDevicesId, data.task_name);
-      } else {
-        return false;
-      }
-      console.log(`Sent chunk ${i} to ${dev.id}`);
+    const data1 = {
+      task_id: data.task_name,
+      number: dev.number,
+      metadata_url: metadataUrl,
+      chunk_url: chunkUrl,
+      data_type: "csv",
+      data_type_params: "a=k",
+      devices_list: otherDevicesList,
+      is_tf_lite: data.is_tf_lite,
+      tf_lite_metadata: metadataTFLite,
+    };
+    //console.log("data1",data1)
+    if (socket) {
+      // Send the work message
+      const msg = new WorkMessage(socket, JSON.stringify(data1));
+      msg.handle();
+      await deviceRepo.setStatus(dev.id, "busy");
+      // construct device mesh of network
+      await deviceRepo.connectDeviceToTask(dev.id, data.task_name, dev.number);
+      // connect working devices to their task
+      const otherDevicesId: string[] = devicesList.filter((d: any) => d.id !== dev.id).map((d: any) => d.id);
+      await deviceRepo.makeDevicesMesh(dev.id, otherDevicesId, data.task_name);
+    } else {
+      return false;
     }
-    await taskRepo.updateTask(data.task_name, "ongoing");
-    
-  
+    console.log(`Sent chunk ${i} to ${dev.id}`);
+  }
+  await taskRepo.updateTask(data.task_name, "ongoing");
 }
